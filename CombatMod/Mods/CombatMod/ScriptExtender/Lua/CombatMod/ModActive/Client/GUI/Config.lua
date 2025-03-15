5faf13b699fbfdbd8dcbf81a3ca7b295c9b2d9b6
celerev <celerev>
2025-03-15 13:31:25 -0700

    'a'

--- CombatMod/Mods/CombatMod/ScriptExtender/Lua/CombatMod/ModActive/Client/GUI/Config.lua
+++ CombatMod/Mods/CombatMod/ScriptExtender/Lua/CombatMod/ModActive/Client/GUI/Config.lua
@@ -16,7 +16,7 @@ function Config.Main(tab)
 
     Config.Checkbox(root, "Challenge Mode", "A brutal challenge. Increases Encounter Budget, increases rate of Enemy Scaling, and allows more powerful monsters to arrive sooner.", "HardMode")
 
-    Config.Checkbox(root, "Lone Wolf Mode", "BETA: A scenario rebalanced for solo players. Press ResetTemplates button at bottom of page after enabling.", "LoneWolfMode")
+    Config.Checkbox(root, "Lone Wolf Mode", "BETA: A scenario rebalanced for solo players. Press ResetTemplates button at bottom of page after enabling or disabling.", "LoneWolfMode")
 
     Config.Checkbox(
         root,
